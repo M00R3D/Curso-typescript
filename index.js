bf3dee2e078d6860d5d72fd51a147c0635d2e581
job moore <jobmurdan@ohtmail.com>
2024-08-20 00:43:25 -0700

    first push

---
+++ index.js
@@ -0,0 +1,3 @@
+var name = "job";
+var age = 20;
+console.log("hello i am " + name + " and my age is " + age);
---
+++ index.ts
@@ -0,0 +1,3 @@
+const name= "job";
+const age= 20;
+console.log("hello i am "+ name +" and my age is " + age);
