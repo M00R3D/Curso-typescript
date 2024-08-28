cf7bfacf54e71498287aa3a24fcaee6c739d94fa
job moore <jobmurdan@ohtmail.com>
2024-08-28 15:57:22 -0700

    ejemplo de uso de las interfaces e implementacion de extends e implements

--- interfaces.ts
+++ interfaces.ts
@@ -3,30 +3,58 @@ interface Book
     id:number;
     title:string;
     description:string;
+    coAuthor?:string;
+    isLoan: (id: number)=> void
 }
 
 const book: Book =
 {
-    id:1,
-    title:"titulo",
-    description:"esta es la descripcion que describe el libro"
+    id: 1,
+    title: "titulo",
+    description: "esta es la descripcion que describe el libro",
+    isLoan: function (id: number): void {
+        throw new Error("Function not implemented.");
+    }
 }
 
 const books: Book[] = [];
+const anotherBook = getBook();
+books.push(anotherBook);
+books.push(anotherBook);
+books.push(anotherBook);
+books.push(anotherBook);
+console.log(books);
 
-function getBook():Book {
-    return {id:1,title:'Titulo',description:"esta es la descripcion que describe el libro"}
+
+function getBook(): Book {
+    return {
+        id: 1,
+        title: 'Titulo',
+        description: "esta es la descripcion que describe el libro",
+        isLoan: function (id: number): void {
+            throw new Error("Function not implemented.");
+        }
+    };
 }
 
 const myBook = getBook()
-
-function createBook(book:Book):Book= {
+function createBook(book:Book):Book {
     return book;   
 }
+console.log(myBook.title);
 
+const newBook: Book = {
+    id: 2,
+    title: "Nuevo Libro",
+    description: "Descripción del nuevo libro",
+    isLoan: function (id: number): void {
+        console.log(`Libro con ID ${id} ha sido prestado.`);
+    }
+};
 
-
-
+const createdBook = createBook(newBook);
+console.log(createdBook.title); 
+createdBook.isLoan(createdBook.id);
 
 
 
@@ -47,4 +75,24 @@ interface Employee extends Person
 interface User extends Person
 {
     money:number;
+}
+
+interface Animal{
+    name: string;
+    getDogs()=>void;
+    getCats()=>void;
+}
+
+
+
+
+
+class Zoo implements Animal{
+    name="mooh  "
+    getDogs():void{
+
+    }
+    getCats():void{
+        
+    }
 }
