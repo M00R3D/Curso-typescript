52219444e2129fa01b130b5f081f547b48020248
job moore <jobmurdan@ohtmail.com>
2024-09-04 15:35:38 -0700

    clases

--- clases.ts
+++ clases.ts
@@ -1,20 +1,34 @@
-class Employee {
-    id:number;
-    name:string;
-    dept:string;
+class Name {
+    public zone= "LatinoAmerica";
+    protected city= "La Paz";
+    private country= "M-X-  ";
+    constructor(){}
 
-    constructor(id:number,name:string,dept:string)
+    greet():void {
+        console.log("hola mundo");
+        
+    }
+}
+
+class Employee extends Name{
+    getEmployeeInfo(): any {
+        console.log("Method not implemented.");
+    }
+
+    constructor(private readonly id:number,private readonly name:string,private readonly dept:string)
     {
-        this.id=id;
-        this.name=name;
-        this.dept=dept;
+        super()
         this.showInfo();
     }
-    showInfo() void{
+    showInfo(): void{
         console.log('${this.name} ${this.dept}');
         
     }
 }
 
 const emp = new Employee(1,"Job","Programacion");
-emp.
+
+emp.greet(); 
+emp.showInfo();
+console.log(emp.getEmployeeInfo()); 
+console.log(emp.zone);
