801f150cf4a48a7c04520c36fdd66c374486acc3
job moore <jobmurdan@ohtmail.com>
2024-09-06 15:46:01 -0700

    generics

---
+++ generics/src/index.ts
@@ -0,0 +1,47 @@
+interface Person{
+    id: number;
+    name: string;
+}
+
+class People{
+    private items:Person[]=[];
+
+    addItem(newItem:Person):void{
+        this.items.push(newItem);
+    }
+    getItems():void{
+        console.log("lista de items "+JSON.stringify(this.items));
+    }
+    getNames():string[]{
+        return this.items.map((item)=>item.name);
+    }
+    getItemById(id:number):Person | undefined{
+        return this.items.find((item:Person)=>item.id===id);
+    }
+}
+
+const personCollection = new People();
+const newData={
+    id:1,
+    name:'Job Moore'
+}
+
+
+////hasta aqui todo normal, hemos creado una  Interfaz Person
+//tambien una Clase People:
+// Esta clase es responsable de gestionar una colección de objetos Person.
+// Contiene una propiedad privada items, que es un array de personas.
+
+
+//Métodos para recibir los parametros de la clase y visualizarlos como en  getItems getNames getItemById o modificarlos como en addItem
+
+
+
+//para poder crear una clase que tenga literalmente lo mismo pero nos sirva para almacenar productos en vez de personas podriamos utilizar un data type
+
+
+interface Product{
+    id: number;
+    name: string;
+    price: number;
+}
